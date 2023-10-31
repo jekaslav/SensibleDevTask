b65f4e43cc2b59d0d34f89c8d46a4d6f3ac87553
jekaslav <evgenevgen8585@gmail.com>
2023-11-01 01:58:17 +0400

    minor fix

--- SensibleDevTask/Services/FileComparison.cs
+++ SensibleDevTask/Services/FileComparison.cs
@@ -6,21 +6,23 @@ namespace SensibleDevTask.Services
 {
     public static class FileComparison
     {
+        private static readonly PersonEntityComparer PersonEntityComparer = new();
+        
         public static List<PersonEntity> GetAddedRecords(List<PersonEntity> records1, List<PersonEntity> records2)
         {
-            var addedRecords = records2.Except(records1, new PersonEntityComparer()).ToList();
+            var addedRecords = records2.Except(records1, PersonEntityComparer).ToList();
             return addedRecords;
         }
 
         public static List<PersonEntity> GetDeletedRecords(List<PersonEntity> records1, List<PersonEntity> records2)
         {
-            var deletedRecords = records1.Except(records2, new PersonEntityComparer()).ToList();
+            var deletedRecords = records1.Except(records2, PersonEntityComparer).ToList();
             return deletedRecords;
         }
 
         public static List<PersonEntity> GetModifiedRecords(List<PersonEntity> records1, List<PersonEntity> records2)
         {
-            var modifiedRecords = records2.Intersect(records1, new PersonEntityComparer())
+            var modifiedRecords = records2.Intersect(records1, PersonEntityComparer)
                 .Where(r => !r.Equals(records1.Single(p => p.Id == r.Id))).ToList();
             return modifiedRecords;
         }
